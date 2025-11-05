b3abc50ac7008531ebab8578a72be4a75d1def04
Erik Siemsen <eflores-siemsen@csuchico.edu>
2025-11-05 11:40:44 -0800

    Unit Tests Created

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,34 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+TEST(PasswordTest, all_same)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaaaaaaaaa");
+	ASSERT_EQ(10, actual);
+}
+TEST(PasswordTest, special_character)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters(";;pplpsalkppat");
+	ASSERT_EQ(2, actual);
+}
+TEST(PasswordTest, alternating_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("atatatatata");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, case_sensitive)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("BBBBbbbbbubbbbles");
+	ASSERT_EQ(4, actual);
+}
+TEST(PasswordTest, null_input)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
